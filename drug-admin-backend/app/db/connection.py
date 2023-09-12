aace2b37319c5ab5a3e8a59dc169154d1bc2c2ff
melissawebster <imelissawebster@gmail.com>
2023-09-12 12:18:02 -0300

    fix database url

--- drug-admin-backend/app/db/connection.py
+++ drug-admin-backend/app/db/connection.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///drugs-database.db"
-
-Session = sessionmaker(bind=create_engine(DATABASE_URL))
+DATABASE_URL = "sqlite:///app/db/database"
 
 Engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(bind=Engine)
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
