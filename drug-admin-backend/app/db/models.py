dd66fb3603d2fc2bed44030ccba4eebd24e45486
melissawebster <imelissawebster@gmail.com>
2023-09-18 19:33:22 -0300

    add insert method

--- drug-admin-backend/app/db/models.py
+++ drug-admin-backend/app/db/models.py
@@ -1,11 +1,25 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy import Column, Integer, Float, String, Boolean
+from .connection import SessionLocal
+from typing import Self
+
 
 Base = declarative_base()
 
 class Drug(Base):
     __tablename__ = 'drug'
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
-    stock = Column(Integer, nullable=False)
+    stock = Column(Boolean, nullable=False)
+
+    def insert(self) -> Self:
+        """
+        Use only for insert operation
+        """
+        session = SessionLocal()
+        try:
+            session.add(self)
+            session.commit()
+        finally:
+            session.close()
