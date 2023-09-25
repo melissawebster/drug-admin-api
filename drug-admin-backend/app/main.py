38980afb2de773e53756d5935752da5e28f7af95
melissawebster <imelissawebster@gmail.com>
2023-09-25 16:10:07 -0300

    set axios

--- drug-admin-backend/app/main.py
+++ drug-admin-backend/app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
-    allow_origins=["http://localhost:3000"]
+    allow_origins=["http://localhost:5173"]
 )
 
 @app.get("/")
