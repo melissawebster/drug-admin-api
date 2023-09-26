9e106956ea17409b3b5480f35551c967dbdf6c9a
melissawebster <imelissawebster@gmail.com>
2023-09-26 11:35:19 -0300

    app background

--- drug-admin-frontend/src/App.tsx
+++ drug-admin-frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import Header from './components/header/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 
 function App() {
-
   return (
     <>
-      <div>
+      <div className="bg-color">
         <Header />
       </div>
 
